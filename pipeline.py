f025c7efd89e11d609e23c0f8d1bd7c3e80f5dc1
Dmitri Babaev <dmitri.babaev@gmail.com>
2019-08-11 03:04:08 +0300

    no dstools dependency

--- pipeline.py
+++ pipeline.py
@@ -1,25 +1,26 @@
 import pandas as pd
 import numpy as np
+from sklearn.base import RegressorMixin, BaseEstimator
+from sklearn.externals.joblib import Parallel
+from sklearn.impute import SimpleImputer
 
-from sklearn.model_selection import cross_val_score, StratifiedKFold, train_test_split
+from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.decomposition import PCA
 from sklearn.decomposition.truncated_svd import TruncatedSVD
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.ensemble.bagging import BaggingRegressor
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_selection.univariate_selection import SelectKBest, f_regression
-from sklearn.gaussian_process import GaussianProcess
+from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.linear_model import Ridge
 from sklearn.metrics import make_scorer
-from sklearn.mixture.gmm import GMM
+from sklearn.mixture import GaussianMixture
 from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import Imputer
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing.data import PolynomialFeatures
+from xgboost import XGBRegressor
 
-import ds_tools.dstools.ml.transformers as tr
-import ds_tools.dstools.ml.xgboost_tools as xgb
 
 def update_model_stats(stats_file, params, results):
     import json
@@ -66,6 +67,44 @@ def init_params(overrides):
         'est_type': 'xgboost',
     }
     return {**defautls, **overrides}
+
+
+def predict_with_id(est, fdf):
+    return pd.Series(est.predict(fdf), index=fdf.index)
+
+
+def fit_clone_with_key(estimator, features, labels, key):
+    from sklearn.base import clone as sk_clone
+    return key, sk_clone(estimator).fit(features, labels)
+
+
+class PerGroupRegressor(RegressorMixin, BaseEstimator):
+    def __init__(self, estimator, split_condition, n_jobs=1, verbose=0):
+        self.split_condition = split_condition
+        self.estimator = estimator
+        self.group_estimators = None
+        self.n_jobs = n_jobs
+        self.verbose = verbose
+
+    def fit(self, X, y):
+        self.group_estimators = dict(Parallel(n_jobs=self.n_jobs)(
+            (
+                (fit_clone_with_key, [self.estimator, gdf, y.ix[gdf.index], gkey], {})
+                for gkey, gdf in X.groupby(self.split_condition)
+            )
+        ))
+
+        return self
+
+    def predict(self, X):
+        preds = pd.concat(Parallel(n_jobs=self.n_jobs, verbose=self.verbose)(
+            (
+                (predict_with_id, [self.group_estimators[gkey], gdf], {})
+                for gkey, gdf in X.groupby(self.split_condition)
+            )
+        ))
+
+        return preds
 
 
 def mape(y_true, y_pred):
@@ -131,10 +170,10 @@ def drop_transform(x):
     return x.drop(['memType', 'os', 'cpuFull', 'cpuArch'], axis=1)
 
 
-def create_union_transf(params):
+def create_union_transf(_):
     pca2c_transformer = make_pipeline(
-        drop_transformer,
-        Imputer(),
+        drop_transform,
+        SimpleImputer(),
         StandardScaler(),
         PCA(n_components=2),
     )
@@ -148,15 +187,15 @@ def create_union_transf(params):
     arch_transformer = FunctionTransformer(lambda x: pd.get_dummies(x.cpuArch), validate=False)
 
     gmm_transformer = make_pipeline(
-        drop_transformer,
-        Imputer(),
+        drop_transform,
+        SimpleImputer(),
         StandardScaler(),
         PCA(n_components=2),
-        FunctionTransformer(lambda x: GMM(n_components=3).fit_predict(x)[np.newaxis].T)
+        FunctionTransformer(lambda x: GaussianMixture(n_components=3).fit_predict(x)[np.newaxis].T)
     )
 
     transf = make_union(
-        drop_transformer,
+        drop_transform,
         gmm_transformer,
         os_transformer,
         arch_transformer,
@@ -167,59 +206,64 @@ def create_union_transf(params):
 
 def create_xgb_est(params):
     keys = {
-        'eta',
+        'learning_rate',
         'min_child_weight',
         'subsample',
         'colsample_bytree',
         'max_depth',
-        'num_rounds',
-        'num_es_rounds',
-        'es_share',
-        'num_parallel_tree'
+        'n_estimators',
     }
     
-    xgb_params = {k: v for k, v in params.items() if k in keys}
-
-    xgb_params_all = {
+    xgb_params = {
         "objective": "reg:linear",
-        "silent": 0,
-        'verbose': 10,
-        'eval_func': mape_evalerror,
-        'ybin_func': ybin,
-        **xgb_params
+        **{k: v for k, v in params.items() if k in keys}
     }
     
     objective = params['objective']
     if objective == 'mape':
-        xgb_params_all['objective_func'] = mape_obj
+        xgb_params['objective'] = mape_obj
     elif objective == 'poisson':
-        xgb_params_all['objective'] = 'count:poisson'
+        xgb_params['objective'] = 'count:poisson'
+
+
+    class XGBC(XGBRegressor):
+        def fit(self, x, y, **kwargs):
+            f_train, f_val, t_train, t_val = train_test_split(x, y, test_size=params['es_share'])
+            super().fit(
+                f_train,
+                t_train,
+                eval_set=[(f_val, t_val)],
+                eval_metric=mape_evalerror,
+                early_stopping_rounds=params['num_es_rounds'],
+                verbose=10)
         
-    return xgb.XGBoostRegressor(**xgb_params_all)
+    return XGBC(**xgb_params)
 
 
 def create_gblinear_est(params):
     keys = {
-        'eta',
-        'num_rounds',
-        'num_es_rounds',
-        'es_share',
-        'lambda',
-        'alpha',
+        'learning_rate',
+        'n_estimators',
     }
     
-    xgb_params = {k: v for k, v in params.items() if k in keys}
-    
-    xgb_params_all = {
+    xgb_params = {
         "booster": 'gblinear',
         "objective": "reg:linear",
-        "silent": 0,
-        'verbose': 10,
-        'eval_func': mape_evalerror,
-        **xgb_params
+        **{k: v for k, v in params.items() if k in keys}
     }
+
+    class XGBC(XGBRegressor):
+        def fit(self, x, y, **kwargs):
+            f_train, f_val, t_train, t_val = train_test_split(x, y, test_size=params['es_share'])
+            super().fit(
+                f_train,
+                t_train,
+                eval_set=[(f_val, t_val)],
+                eval_metric=mape_evalerror,
+                early_stopping_rounds=params['num_es_rounds'],
+                verbose=10)
         
-    return xgb.XGBoostRegressor(**xgb_params_all)
+    return XGBC(**xgb_params)
 
 
 def validate(params):    
@@ -229,32 +273,34 @@ def validate(params):
         transf = FunctionTransformer(drop_transform, validate=False)
     elif transf_type == 'dr+inp+sc+pca':
         transf = make_pipeline(
-            drop_transformer,
-            Imputer(),
+            drop_transform,
+            SimpleImputer(),
             StandardScaler(),
             PCA(n_components=params['n_pca_components']),
         )
     elif transf_type == 'dr+inp':
         transf = make_pipeline(
-            drop_transformer,
-            Imputer(),
+            drop_transform,
+            SimpleImputer(),
         )
     elif transf_type == 'dr+inp+sc':
         transf = make_pipeline(
-            drop_transformer,
-            Imputer(),
+            drop_transform,
+            SimpleImputer(),
             StandardScaler()
         )
     elif transf_type == 'union':
         transf = create_union_transf(params)
     elif transf_type == 'poly_kbest':
         transf = make_pipeline(
-            drop_transformer,
-            Imputer(),
+            drop_transform,
+            SimpleImputer(),
             StandardScaler(),
             PolynomialFeatures(degree=2, interaction_only=True),
             SelectKBest(f_regression, params['best_features']),
         )
+    else:
+        raise AttributeError(f'unknown transformer type: {transf_type}')
         
     est_type = params['est_type']
 
@@ -265,9 +311,11 @@ def validate(params):
     elif est_type == 'exttree':
         est = ExtraTreesRegressor(n_estimators=params['n_estimators'], n_jobs=-1)
     elif est_type == 'gp':
-        est = GaussianProcess(theta0=params['theta0'])
+        est = GaussianProcessRegressor()
     elif est_type == 'ridge':
         est = Ridge(alpha=params['alpha'])
+    else:
+        raise AttributeError(f'unknown estimator type: {est_type}')
         
     if params['bagging']:
         BaggingRegressor(
@@ -279,7 +327,7 @@ def validate(params):
     pl = make_pipeline(transf, est)
             
     if params['per_group_regr']:
-        pl = tr.PerGroupRegressor(
+        pl = PerGroupRegressor(
             estimator=pl,
             split_condition=['os', 'cpuFreq', 'memSize_MB'],
             n_jobs=1,
